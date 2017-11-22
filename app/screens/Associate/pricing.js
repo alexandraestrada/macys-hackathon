e88d2d523952a147cc4b002fb6ef308729a301f3
Katie Lin <katie.lin@macys.com>
2017-11-21 22:56:28 -0800

    changing button state

--- app/screens/Associate/pricing.js
+++ app/screens/Associate/pricing.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Text,
   View,
-  StatusBar
+  StatusBar,
+  TouchableHighlight,
 } from 'react-native';
 import { Container, Header, Content, Footer, FooterTab, Button, Body, Title, List, ListItem, Thumbnail, Left, Right, Input, Item} from 'native-base';
 
@@ -15,26 +16,45 @@ export interface State {}
 class Pricing extends React.Component<Props, State> {
 
 	constructor() {
-      super()
-      this.state = {
-         myText: 'Enter details of your question...'
-      }
-      this.socket = SocketIOClient('https://young-brook-73094.herokuapp.com');
-      this.socket.on('questionSubmitted', ({ question }) => 
-        this.props.navigation.navigate('New_Question', { question }));
-   	}
+    super()
+    this.state = {
+       myText: 'Enter details of your question...',
+       buttonStatus: 0
+    }
+    this.socket = SocketIOClient('https://young-brook-73094.herokuapp.com');
+    this.socket.on('questionSubmitted', ({ question }) => 
+      this.props.navigation.navigate('New_Question', { question }));
+ 	}
 
-    updateTextNeedManager = () => {
-      this.setState({ myText: 'I need a price override' })
-   	}
+  pressButtonOne = () => {
+    this.setState({ buttonStatus: 1,
+                    myText: 'I need a price override'
+    })
+  }
 
-   	updateTextCouponExemption = () => {
-   		this.setState({myText: 'I need to apply a coupon exemption'})
-   	}
+  pressButtonTwo = () => {
+    this.setState({ buttonStatus: 2,
+                    myText: 'I need to apply a coupon exemption'
+    })
+  }
 
-   	updateTextOther = () => {
-   		this.setState({myText: 'Enter details of your question'})
-   	}
+  pressButtonThree= () => {
+    this.setState({ buttonStatus: 3,
+                    myText: 'Enter details of your questions'
+    })
+  }
+
+  updateTextNeedManager = () => {
+    this.setState({ myText: 'I need a price override' })
+ 	}
+
+ 	updateTextCouponExemption = () => {
+ 		this.setState({myText: 'I need to apply a coupon exemption'})
+ 	}
+
+ 	updateTextOther = () => {
+ 		this.setState({myText: 'Enter details of your question'})
+ 	}
 
 	static navigationOptions = () => ({
 	    title: 'Pricing',
@@ -79,15 +99,30 @@ class Pricing extends React.Component<Props, State> {
 				<Content>
 
 					<View style={{flex:1, flexDirection: 'column', justifyContent: 'center', marginLeft: 10, marginTop: 20}}>
-						<Button style={styles.option} >
-							<Text style={styles.optionText} onPress = {this.updateTextNeedManager}>I need a price override</Text>
-						</Button>
-						<Button style={styles.option} >
-							<Text style={styles.optionText} onPress = {this.updateTextCouponExemption}>I need to apply a coupon exemption</Text>
-						</Button>
-						<Button style={styles.option} onPress = {this.updateTextOther}>
-							<Text style={styles.optionText}>Other</Text>
-						</Button>
+            <TouchableHighlight
+              activeOpacity={1}
+              style={ this.state.buttonStatus === 1 ? styles.buttonPress : styles.button }
+              onPress ={this.pressButtonOne.bind(this)}
+            >
+              <View>
+                <Text style={styles.buttonText}>I need a price override</Text>
+              </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              activeOpacity={1}
+              style={ this.state.buttonStatus === 2 ? styles.buttonPress : styles.button }
+              onPress= {this.pressButtonTwo.bind(this)}
+            >
+							<Text style={styles.buttonText}>I need to apply a coupon exemption</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              style={ this.state.buttonStatus === 3 ? styles.buttonPress : styles.button }
+              onPress = {this.pressButtonThree.bind(this)}
+            >            
+							<Text style={styles.buttonText}>Other</Text>
+						</TouchableHighlight>
 
 						<Item regular style={styles.message}>
 							<Input placeholder={this.state.myText} />
@@ -124,22 +159,39 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  option:{
+  button:{
     backgroundColor: '#F4F6F8',
     height: 60,
-    width: 335,
+    width: 340,
     margin: 10,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
     shadowOffset:{  width: 1,  height: 1,  },
-  shadowColor: 'grey',
-  shadowOpacity: 0.3,
+    shadowColor: 'grey',
+    shadowOpacity: 0.3,
   },
-  optionText: {
-  	fontSize: 20,
-
+  buttonPress: {
+    backgroundColor: '#FBEAE5',
+    borderColor: '#E37572',
+    borderWidth: 1,
+    height: 60,
+    width: 340,
+    shadowOffset:{  width: 1,  height: 1,  },
+    shadowColor: 'grey',
+    shadowOpacity: 0.3,
+    margin: 10,
+    borderRadius: 10,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    
+  },
+  buttonText:{
+    fontSize: 20,
+    marginLeft: 10,
   },
   send: {
   	backgroundColor: '#CC0000',
